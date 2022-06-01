046ae07b874c904fc26fabd945f5a6f05e1f1de9
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2022-06-01 19:40:16 +0200

    CPLP-973: Change to catenax-ng dev keycloak

--- src/services/EnvironmentService.ts
+++ src/services/EnvironmentService.ts
@@ -17,9 +17,7 @@ export const getCentralIdp = () => {
   const hostname = getHostname()
   if (hostname === 'portal.int.demo.catena-x.net')
     return 'https://centralidp.demo.catena-x.net/auth'
-  if (hostname === 'portal.catena-x.net')
-    return 'https://centralidp.catena-x.net/auth'
-  return 'https://catenaxdev003akssrv.germanywestcentral.cloudapp.azure.com/iamcentralidp/auth'
+  return 'https://centralidp.dev.demo.catena-x.net/auth'
 }
 
 const EnvironmentService = {
