9abfc291a3ae25819108db5a02df7f8d578078ac
nidhi garg <nidhi.garg@partner.bmw.in>
2022-12-29 16:39:09 +0530

    reduxe email regex

--- src/components/cax-responsibilities.tsx
+++ src/components/cax-responsibilities.tsx
@@ -104,7 +104,7 @@ export const ResponsibilitiesCax = ({
 
   const validateEmail = (email) =>
     //eslint-disable-next-line
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[.\d{1,3}\])|(([a-z0-9-]+\.)+[a-z]{2,}))$/i.test(email)
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([a-z0-9-]+\.)+[a-z]{2,})$/i.test(email)
 
   const validatePersonalNote = (note: string) =>
     /^[a-zA-Z][a-zA-Z0-9 !#'$@&%()*+,\-_./:;=<>?[\]\\^]{0,255}$/.test(note)
