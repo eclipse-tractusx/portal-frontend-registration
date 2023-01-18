e27c9464f20e7e4a70a8fae83d580b27bd37f0e2
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2023-01-18 13:40:41 +0100

    cplp-1874 - inject dynamic config in index.html

--- scripts/inject-dynamic-env.sh
+++ scripts/inject-dynamic-env.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-custom_env_vars='{"HOSTTYPE":"'$HOSTTYPE'","HOSTTYPE":"hosttype","HOSTTYPE":"hosttype"}'
-custom_env_vars_anchor='<!--CUSTOM_CONFIG_PLACEHOLDER-->'
-index_html_reference=`cat index.html`
+# Copyright (c) 2021,2022,2023 Contributors to the Eclipse Foundation
+
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+custom_env_vars='{"PORTAL_FRONTEND_URL":"'$PORTAL_FRONTEND_URL'","PORTAL_BACKEND_URL":"'$PORTAL_BACKEND_URL'","CENTRALIDP_URL":"'$CENTRALIDP_URL'"}'
+custom_env_vars_anchor='{"PORTAL_FRONTEND_URL":"https://portal.dummy","PORTAL_BACKEND_URL":"https://portal-backend.dummy","CENTRALIDP_URL":"https://centralidp.dummy/auth"}'
+index_html_reference=`cat /usr/share/nginx/html/index.html.reference`
 index_html=${index_html_reference//$custom_env_vars_anchor/$custom_env_vars}
-echo "$index_html" > index2.html
+echo "$index_html" > /usr/share/nginx/html/index.html
+rm /usr/share/nginx/html/index.html.reference
+echo "Environment config successfully injected into index.html"
