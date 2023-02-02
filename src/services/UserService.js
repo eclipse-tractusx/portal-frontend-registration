af70f2680eb45a54d186047f0ec0c949319e2ce2
nidhi garg <nidhi.garg@partner.bmw.in>
2023-02-02 11:43:46 +0530

    remove console

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -57,7 +57,6 @@ const _kc = new Keycloak({
  * @param onAuthenticatedCallback
  */
 const initKeycloak = (onAuthenticatedCallback) => {
-  console.log('window.location.origin', window.location)
   const url = window.location.pathname === '/registration/help' ? '/registration/help' : '/registration/load'
   _kc
     .init({
