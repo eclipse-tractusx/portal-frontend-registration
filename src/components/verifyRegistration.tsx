5108f33f21faf4557231c46931ca21e5f1792a84
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-11-15 09:30:09 +0100

    fix(build): repair broken build

--- src/components/verifyRegistration.tsx
+++ src/components/verifyRegistration.tsx
@@ -251,6 +251,7 @@ export const VerifyRegistration = ({
         handleBackClick={() => backClick()}
         handleNextClick={() => nextClick()}
         tooltip={getTooltip()}
+        helpUrl={'/documentation/?path=docs%2F01.+Onboarding%2F02.+Registration%2F04.+Company+Role+%26+Consent.md'}
       />
     </>
   )
