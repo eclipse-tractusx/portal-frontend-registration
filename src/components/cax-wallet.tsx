8090a02bd8e594329ed7ae1d40b2e05d30f00375
Saloni Garg <saloni.garg@cofinity-x.com>
2025-07-30 09:47:07 +0200

    Update src/components/cax-wallet.tsx
    Co-authored-by: Karsten Thiems <karsten.thiems@cofinity-x.com>

--- src/components/cax-wallet.tsx
+++ src/components/cax-wallet.tsx
@@ -1,7 +1,6 @@
 /********************************************************************************
  * Copyright (c) 2025 Cofinity-X GmbH
  *
- *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
  *
